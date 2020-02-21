63e9a10810f1c6e0b1b4f3e26a9cf66173c10a7a
Loicburnand <loicburnand@gmail.com>
2020-02-21 12:15:43 +0800

    BD + SeedData
    Last save before using IdentityServer4

---
+++ ELearningPlatform/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ELearningPlatform.Data;
+
+namespace ELearningPlatform.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ELearningPlatformContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ELearningPlatformContext>>()))
+            {
+                // Look for any tokens.
+                if (context.Role.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Role.AddRange(
+                    new Role
+                    {
+                        Label = "Guest"
+                    },
+                    new Role
+                    {
+                        Label = "Student"
+                    },
+                    new Role
+                    {
+                        Label = "Instructor"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
